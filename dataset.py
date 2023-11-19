5942722b5267db8e80250d6779e603025e01829d
Mahmoud Magdi <71672057+mahm0udmagdy@users.noreply.github.com>
2023-11-19 22:46:35 +0100

    Create dataset.py
    This script handle and wrap the dataset that is stored in HDF5 file format.

---
+++ dataset.py
@@ -0,0 +1,27 @@
+import lib
+
+class Hfdata (Dataset):
+    def __init__(self, file):
+        self.file = h5py.File(file, 'r')
+        self.data = h5py.file['data'][:]
+        self.labels = h5py.file['label'][:]
+
+    def __getitem__(self, idx):
+        x = self.data[idx]
+        y = self.labels[idx]
+        return x, y
+    
+    def __len__(self):
+        return len(self.data)
+
+data_sp_tr = Hfdata("add your 3D spatial maps training data.h5")
+dataloader_sp_tr = DataLoader(data_sp_tr, batch_size=8, shuffle=True)
+
+data_ts_tr = Hfdata("add your 1D time series training data.h5")
+dataloadert_ts_tr = DataLoader(data_ts_tr, batch_size=8, shuffle=True)
+
+data_sp_val = Hfdata('add your 3D spatial maps Validation data.h5')
+dataloader_sp_val = DataLoader(data_sp_val, batch_size=8, shuffle=False)
+
+data_ts_val = Hfdata('add your 1D time series Validation data.h5')
+dataloader_tm_v = DataLoader(data_ts_val, batch_size=8, shuffle=False)
