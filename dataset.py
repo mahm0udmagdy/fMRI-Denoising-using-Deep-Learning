549262399fc7069cc19da19e287a09c9d2e3b28d
Mahmoud Magdi <71672057+mahm0udmagdy@users.noreply.github.com>
2023-11-19 22:53:06 +0100

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -24,4 +24,26 @@ data_sp_val = Hfdata('add your 3D spatial maps Validation data.h5')
 dataloader_sp_val = DataLoader(data_sp_val, batch_size=8, shuffle=False)
 
 data_ts_val = Hfdata('add your 1D time series Validation data.h5')
-dataloader_tm_v = DataLoader(data_ts_val, batch_size=8, shuffle=False)
+dataloader_ts_v = DataLoader(data_ts_val, batch_size=8, shuffle=False)
+
+class Hfdata_test(Dataset):
+    def __init__(self, data_dir, file_name):
+        self.data_dir = data_dir
+        self.file_name = file_name
+        self.data = []
+        with h5py.File(f"{data_dir}/{file_name}", "r") as f:
+            self.data.append(f["data"][:])
+
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        x = self.data[idx]
+       
+        return x
+    
+folder_path_sp = 'Add your 3D spatial maps testing data'
+file_list_sp = glob.glob(os.path.join(folder_path_sp, '*.h5'))
+folder_path_ts = 'Add your 1D time series testing data'
+file_list_ts = glob.glob(os.path.join(folder_path_ts, '*.h5'))
